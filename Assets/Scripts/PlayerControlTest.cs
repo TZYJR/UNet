257dc2eacf144f8fd8e042ef5fef7f9ae6159b52
YangJiaQi <1295585769@qq.com>
2018-05-26 11:22:45 +0800

    01

---
+++ Assets/Scripts/PlayerControlTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlTest : MonoBehaviour {
+
+	private void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		this.transform.Translate(Vector3.forward * v * 3 * Time.deltaTime);
+		this.transform.Rotate(Vector3.up * h * 120 * Time.deltaTime);
+	}
+}
